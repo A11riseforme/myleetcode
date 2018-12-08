f3d0264bb4c1f915b27d8f59580a3082b2319767
Chenglong Yang <chenglong.yang@u.nus.edu>
2018-12-08 12:50:05 +0800

    slightly changed

--- 13_roman_to_integer.c
+++ 13_roman_to_integer.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-int roman_char_to_num(char c)
-{
+int roman_char_to_num(char c) {
     switch(c) {
     case 'I':
         return 1;
@@ -26,7 +25,7 @@ int romanToInt(char* s) {
     int sum = roman_char_to_num(s[0]);
     for (int i = 1; s[i] != '\0'; i += 1) {
         int current_num = roman_char_to_num(s[i]);
-        int previous_num = roman_char_to_num(s[i-1]);
+        int previous_num = roman_char_to_num(s[i - 1]);
         if (current_num > previous_num) {
             sum = sum - previous_num + (current_num - previous_num);
         } else {
