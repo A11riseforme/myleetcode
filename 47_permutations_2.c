f7bb9fa47eb09ae3002ad1db4ec320fd53fb3a0c
Chenglong Yang <chenglong.yang@u.nus.edu>
2018-12-12 01:07:32 +0800

    optimized a little bit

--- 47_permutations_2.c
+++ 47_permutations_2.c
@@ -7,46 +7,46 @@ int cmp (const void *a, const void *b) {
 }
 
 
-bool isReversed(int *nums, int numsSize) {
-    for (int i = 0; i < numsSize - 1; i++) {
-        if (nums[i] < nums[i + 1]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-
 /**
  * Return an array of arrays of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int** permuteUnique(int* nums, int numsSize, int* returnSize) {
+    qsort(nums, numsSize, sizeof(nums[0]), cmp);
+    int min = nums[0];
+    int max = nums[numsSize - 1];
+    int range = max - min;
+    int *freq = calloc(range + 1, sizeof(int));
+    for (int i = 0; i < numsSize; i++) {
+        freq[nums[i] - min]++;
+    }
     int maxSize = 1;
     int n = numsSize;
     while (n > 0) {
         maxSize *= n;
         n--;
     }
-
-    int **result = calloc(maxSize, sizeof(int *));
-    for (int i = 0; i < maxSize; i++) {
+    for (int i = 0; i <= range; i++) {
+        if (freq[i] > 1) {
+            for (int j = freq[i]; j > 1; j--) {
+                maxSize /= j;
+            }
+        }
+    }
+    *returnSize = maxSize;
+    int **result = calloc(*returnSize, sizeof(int *));
+    for (int i = 0; i < *returnSize; i++) {
         *(result + i) = calloc(numsSize, sizeof(int));
     }
 
-    qsort(nums, numsSize, sizeof(nums[0]), cmp);
     int *initial = calloc(numsSize, sizeof(int));
     memcpy(initial, nums, numsSize * sizeof(int));
-    int i = 0;
-    *returnSize = 0;
-    while (!isReversed(initial, numsSize)) {
+    for (int i = 0; i < *returnSize; i++) {
         memcpy(*(result + i), initial, numsSize * sizeof(int));
         nextPermutation(initial, numsSize);
-        i++;
-        (*returnSize)++;
     }
-    memcpy(*(result + i), initial, numsSize * sizeof(int));
-    (*returnSize)++;
+    free(initial);
+    free(freq);
 
     return result;
 }
@@ -85,7 +85,7 @@ void nextPermutation(int* nums, int numsSize) {
     }
     // if swap_1_position is not found, the original nums is biggest, just reverse it.
     if (swap_1_position == -1) {
-        reverse(nums,0,numsSize-1);
+        reverse(nums, 0, numsSize - 1);
         return;
     }
     // swap the number on two swapping positions.
@@ -94,19 +94,24 @@ void nextPermutation(int* nums, int numsSize) {
     nums[swap_2_position] = temp;
 
     // reverse the numbers on the right of the first swapping position.
-    reverse(nums, swap_1_position+1,numsSize-1);
+    reverse(nums, swap_1_position + 1, numsSize - 1);
     return;
 }
 
 
 int main() {
-    int nums[] = {1,2,2};
+    int nums[] = {1, 2, 2, 3, 3};
     int returnSize;
-    int **result = permuteUnique(nums, 3, &returnSize);
+    int **result = permuteUnique(nums, 5, &returnSize);
+    printf("returnSize is %d\n", returnSize);
     for (int i = 0; i < returnSize; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d ",result[i][j]);
+        for (int j = 0; j < 5; j++) {
+            printf("%d ", result[i][j]);
         }
         printf("\n");
     }
+    for (int i = 0; i < returnSize; i++) {
+        free(*(result + i));
+    }
+    free(result);
 }
