49b443975082901de863e5d0b1506ee9a445d098
Chenglong Yang <chenglong.yang@u.nus.edu>
2018-12-14 08:53:33 +0800

    qn 66

---
+++ 66_plus_one.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int *result = calloc(digitsSize + 1, sizeof(int));
+    int carry = 0, sum, digit;
+    sum = 1 + digits[digitsSize - 1];
+    digit = sum % 10;
+    carry = sum / 10;
+    *(result + digitsSize) = digit;
+    for (int i = digitsSize - 2; i >= 0; i--) {
+        sum = digits[i] + carry;
+        digit = sum % 10;
+        carry = sum / 10;
+        *(result + i + 1) = digit;
+    }
+    if (carry == 1) {
+        *returnSize = digitsSize + 1;
+        *result = 1;
+    } else {
+        for (int i = 0; i < digitsSize; i++) {
+            *(result + i) = *(result + i + 1);
+        }
+        *returnSize = digitsSize;
+    }
+    return result;
+}
+
+
+int main() {
+    int digits[3] = {9,9,9};
+    int returnSize;
+    int *result = plusOne(digits, 3, &returnSize);
+    for (int i = 0; i < returnSize; i++) {
+        printf("%d ",*(result + i));
+    }
+    free(result);
+}
