4ab1f0afd83aca478ef2928f9c9797d2d029c6fb
Chenglong Yang <chenglong.yang@u.nus.edu>
2018-12-15 00:59:40 +0800

    qn 74

---
+++ 74_search_a_2d_matrix.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool searchMatrix(int** matrix, int matrixRowSize, int matrixColSize, int target) {
+    for (int i = 0; i < matrixRowSize; i++) {
+        if ((target >= matrix[i][0] && i == matrixRowSize - 1) ||
+            (target >= matrix[i][0] && target < matrix[i + 1][0])) {
+            if (target <= matrix[i][matrixColSize - 1]) {
+                for (int j = 0; j < matrixColSize; j++) {
+                    if (matrix[i][j] == target) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return false;
+}
+
+
+int main() {
+    int **matrix = calloc(3, sizeof(int *));
+    int a[] = {1, 3, 5, 7};
+    int b[] = {10, 11, 16, 20};
+    int c[] = {23, 30, 34, 50};
+    *(matrix + 0 ) = a;
+    *(matrix + 1 ) = b;
+    *(matrix + 2 ) = c;
+    int target = 16;
+    if (searchMatrix(matrix, 3, 4, target)) {
+        printf("%d is contained in the matrix", target);
+    } else {
+        printf("%d is not contained in the matrix", target);
+    }
+    free(matrix);
+}
