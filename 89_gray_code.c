67703511ee59165bbfa5a824fa20b52b3248419c
Chenglong Yang <chenglong.yang@u.nus.edu>
2018-12-17 01:39:31 +0800

    qn 89

---
+++ 89_gray_code.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ * PS. Honestly I don't know how this works, I just checked how graycode is generated.
+ */
+int* grayCode(int n, int* returnSize) {
+    *returnSize = (int)pow(2, n);
+    int *result = calloc(*returnSize, sizeof(int));
+    for (int i = 0; i < *returnSize; i++) {
+        *(result + i) = (i ^ (i >> 1)); // G(n) = B(n)^B(n/2)
+    }
+    return result;
+}
+
+
+int main() {
+    int n = 4;
+    int returnSize;
+    int *result = grayCode(n, &returnSize);
+    for (int i = 0; i < returnSize; i++) {
+        char s[5];
+        itoa(result[i], s, 2);
+        printf("%04s\n", s);
+    }
+}
