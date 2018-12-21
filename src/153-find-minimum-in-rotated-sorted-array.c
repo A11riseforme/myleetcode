1129def5f8a4b37cde0b6c349e4683cbdda9977f
Chenglong Yang <chenglong.yang@u.nus.edu>
2018-12-21 13:08:24 +0800

    qn 153

---
+++ src/153-find-minimum-in-rotated-sorted-array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int findMin(int* nums, int numsSize) {
+    int start = 1;
+    int end = numsSize - 1;
+    int min = nums[0];
+    while (start <= end) {
+        int mid = (end + start) / 2;
+        min = min < nums[mid] ? min : nums[mid];
+        if (nums[mid] > nums[end]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return min;
+}
+
+
+int main(int argc, char **argv) {
+    int nums[6] = {5,6,1,2,3,4};
+    int test[] = {1,2,3,4,5,6,7,8,9};
+    int min = findMin(nums, 6);
+    printf("%d", min);
+}
