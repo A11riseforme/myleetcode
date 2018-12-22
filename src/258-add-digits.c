f7d7790e1475b2c15b3585a2d1dcb74ef307d3af
Chenglong Yang <chenglong.yang@u.nus.edu>
2018-12-22 19:07:27 +0800

    qn 258

---
+++ src/258-add-digits.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// check https://en.wikipedia.org/wiki/Digital_root#Congruence_formula
+int addDigits(int num) {
+    return 1 + (num - 1) % 9;
+}
+
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: ./test number\n");
+        exit(-1);
+    }
+
+    printf("%d\n", addDigits(atoi(argv[1])));
+    return 0;
+}
