8231c1b723f0649dd5b83092f476b27c060aa38a
Chenglong Yang <chenglong.yang@u.nus.edu>
2018-12-24 23:36:53 +0800

    improve algorithm, beat 100%

--- src/219-contains-duplicate-ii.c
+++ src/219-contains-duplicate-ii.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 
 // can be optimized using hash table or sliding window.
-bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
+bool containsNearbyDuplicateSlow(int* nums, int numsSize, int k) {
     if(numsSize == 1) {
         return false;
     }
@@ -11,6 +11,41 @@ bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
         for(int j = i + 1; (j <= i + k) && (j < numsSize); j++) {
             if(nums[i] == nums[j])
                 return true;
+        }
+    }
+    return false;
+}
+
+// using counting sort.
+bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
+    if(numsSize == 1) {
+        return false;
+    }
+    int min = nums[0];
+    int max = nums[0];
+    for (int i = 0; i < numsSize; i++) {
+        min = nums[i] < min ? nums[i] : min;
+        max = nums[i] > max ? nums[i] : max;
+    }
+    int *tables = malloc((max - min + 1) * sizeof(int));
+    for (int i = 0; i < max - min + 1; i++) {
+        tables[i] = -1;
+    }
+    for (int i = 0; i < numsSize; i++) {
+        // never appear before.
+        if (tables[nums[i] - min] == -1) {
+            tables[nums[i] - min] = i;
+            continue;
+        }
+        if (tables[nums[i] - min] != -1) {
+            // if interval is less than or equal to k.
+            if (i - tables[nums[i] - min] <= k) {
+                return true;
+            }
+            else {
+                // record down last index of i.
+                tables[nums[i] - min] = i;
+            }
         }
     }
     return false;
