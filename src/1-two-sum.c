b2885611a22ee063cf0e5181a8f22db6c7e9dac8
Chenglong Yang <chenglong.yang@u.nus.edu>
2018-12-25 23:14:02 +0800

    update algorithm of hash table

--- src/1-two-sum.c
+++ src/1-two-sum.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+    int value;
+    int index;
+} table;
+
+
+int compare(const void *a, const void *b){
+    return ((table *) a)->value - ((table *) b)->value;
+}
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ * counting sort, hash table.
+ */
 int *twoSum(int numbers[], int n, int target) {
+    table *tables = malloc(n * sizeof(*tables));
+    for (int i = 0; i < n; i++) {
+        tables[i].value = numbers[i];
+        tables[i].index = i;
+    }
+    qsort(tables, n, sizeof(*tables), compare);
+    int left = 0;
+    int right = n - 1;
+    int diff;
+    int *results = malloc(2 * sizeof(int));
+    while (left < right) {
+        int diff = target - tables[left].value;
+        // if sum is too small -> left number is too small.
+        if (diff > tables[right].value) {
+            // move the left pointer to the right, until meet a left number with different bigger value.
+            while (++left < right && tables[left].value == tables[left - 1].value) {}
+        // if sum is too big -> right number is too big.
+        } else if (diff < tables[right].value) {
+            // move the right pointer to the left, until meet a right number with different smaller value.
+            while (--right > left && tables[right].value == tables[right + 1].value) {}
+        } else {
+            results[0] = tables[left].index;
+            results[1] = tables[right].index;
+            free(tables);
+            return results;
+        }
+    }
+    free(results);
+    free(tables);
+    return NULL;
+}
+
+
+// brute force
+int *twoSumSlow(int numbers[], int n, int target) {
     int index1;
     int index2;
     int* ret = (int*)malloc(sizeof(int) * 2);
