067684c00f843af0a034e9e57459fb91498bd826
Chenglong Yang <chenglong.yang@u.nus.edu>
2018-12-31 17:05:01 +0800

    fix memory leak

--- src/461-hamming-distance.c
+++ src/461-hamming-distance.c
@@ -3,23 +3,25 @@
 
 // straight-forward solution.
 int hammingDistance(int x, int y) {
-    char *xString = calloc(32, sizeof(char));
-    char *yString = calloc(32, sizeof(char));
+    int *xString = calloc(32, sizeof(int));
+    int *yString = calloc(32, sizeof(int));
     int tmp;
-    for (int i = 31; i >= 0; i--) {
+    for (int i = 31; x != 0; i--) {
         tmp = x % 2;
-        xString[i] = tmp + '0';
+        xString[i] = tmp;
         x /= 2;
     }
-    for (int i = 31; i >= 0; i--) {
+    for (int i = 31; y != 0; i--) {
         tmp = y % 2;
-        yString[i] = tmp + '0';
+        yString[i] = tmp;
         y /= 2;
     }
     int result = 0;
     for (int i = 0; i < 32; i++) {
         result += xString[i] == yString[i] ? 0 : 1;
     }
+    free(xString);
+    free(yString);
     return result;
 }
 
