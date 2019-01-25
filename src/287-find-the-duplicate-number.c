85ce83d73ec5978e3d907113180ccd2e96658b1f
Chenglong Yang <chenglong.yang@u.nus.edu>
2019-01-25 23:22:34 +0800

    update 287-find-the-duplicate-number.c using Floyd's Tortoise and Hare(cycle detection) algorithm

--- src/287-find-the-duplicate-number.c
+++ src/287-find-the-duplicate-number.c
@@ -17,6 +17,28 @@ int findDuplicate(int* nums, int numsSize) {
     return 0;
 }
 
+// Floyd's Tortoise and Hare(cycle detection) algorithm.
+int findDuplicate(int* nums, int numsSize) {
+    if (numsSize > 1) {
+        // 快慢指针
+        int slow = nums[0]; // 慢指针
+        int fast = nums[nums[0]]; // 快指针
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        // 快慢指针相遇
+        fast = 0; //再来一遍
+        while (fast != slow) {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return slow;
+    }
+    return -1;
+}
+
+
 int main() {
     int nums[] = {1,3,4,2,2};
     printf("%d", findDuplicate(nums, 5));
