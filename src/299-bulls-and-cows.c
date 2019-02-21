7cf256fa16402ec202958b478e661509b25366ab
Chenglong Yang <chenglong.yang@u.nus.edu>
2019-02-22 00:39:11 +0800

    improved 299-bulls-and-cows.c using one pass

--- src/299-bulls-and-cows.c
+++ src/299-bulls-and-cows.c
@@ -11,22 +11,23 @@
 char* getHint(char* secret, char* guess) {
     char *result = malloc(20 * sizeof(char));
     int bull = 0, cow = 0;
-    int *freq1 = calloc(10, sizeof(int));
-    int *freq2 = calloc(10, sizeof(int));
+    int *freq = calloc(10, sizeof(int));
     for (int i = 0; secret[i] != '\0'; ++i) {
         if (secret[i] == guess[i]) {
             ++bull;
         } else {
-            ++freq1[secret[i] - '0'];
-            ++freq2[guess[i] - '0'];
+            if (freq[secret[i] - '0'] > 0) {
+                ++cow;
+            }
+            if (freq[guess[i] - '0'] < 0) {
+                ++cow;
+            }
+            --freq[secret[i] - '0'];
+            ++freq[guess[i] - '0'];
         }
     }
-    for (int i = 0; i < 10; ++i) {
-        cow += MIN(freq1[i], freq2[i]);
-    }
     sprintf(result, "%dA%dB", bull, cow);
-    free(freq1);
-    free(freq2);
+    free(freq);
     return result;
 }
 
