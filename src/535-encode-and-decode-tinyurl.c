b8d985062423d90e748d57fc9c2629ed3529b758
Chenglong Yang <chenglong.yang@u.nus.edu>
2019-04-30 15:59:54 +0800

    qn 535

---
+++ src/535-encode-and-decode-tinyurl.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// should have implemented hash tables....
+/** Encodes a URL to a shortened URL. */
+char *encode(char *longUrl) { return longUrl; }
+
+/** Decodes a shortened URL to its original URL. */
+char *decode(char *shortUrl) { return shortUrl; }
+
+// Your functions will be called as such:
+// char* s = encode(s);
+// decode(s);
+
+int main(int argc, char **argv) {
+    /*
+    if (argc != 2) {
+        fprintf(stderr, "Usage: ./test number ...\n");
+        exit(-1);
+    }
+    */
+    printf("I am too lazy to code the testcases...\n");
+    return 0;
+}
