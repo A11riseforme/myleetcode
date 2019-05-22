605cd7e8d73dad1848cb37652f4b59a4a3fb33ae
A11riseforme <ycl0217@hotmail.com>
2019-05-22 20:15:33 +0800

    some changes

--- template.c
+++ template.c
@@ -1,21 +1,17 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include <ctype.h>
 #include <limits.h>
 #include <math.h>
-#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 
     if (argc != 2) {
         fprintf(stderr, "Usage: ./test number ...\n");
         exit(-1);
     }
 
-    int result = countAndSay(atoi(argv[1]));
-    printf("%s\n", result);
     return 0;
 }
